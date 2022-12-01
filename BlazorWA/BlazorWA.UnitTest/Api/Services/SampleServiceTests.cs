550d4bba843a49c6f6ba07392fb9e26828f4dbd3
phetanshi <padmasekhar.pottepalem@outlook.com>
2022-12-01 21:17:54 +0530

    added unit test file for API

---
+++ BlazorWA/BlazorWA.UnitTest/Api/Services/SampleServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using BlazorWA.Api.Services.Interfaces;
+using BlazorWA.Api.Services.Definitions;
+
+namespace BlazorWA.UnitTest.Api.Services
+{
+    public class SampleServiceTests
+    {
+
+    }
+}
