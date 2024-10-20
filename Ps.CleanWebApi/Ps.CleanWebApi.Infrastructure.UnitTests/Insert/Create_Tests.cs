089526050c90bacf744804ce8d9ab31050129493
phetanshi <padmasekhar.pottepalem@gmail.com>
2024-10-20 15:28:08 +0530

    Changes

---
+++ Ps.CleanWebApi/Ps.CleanWebApi.Infrastructure.UnitTests/Insert/Create_Tests.cs
@@ -0,0 +1,20 @@
+﻿namespace Ps.CleanWebApi.Infrastructure.UnitTests.Insert;
+
+public class Create_Tests
+{
+    [Fact]
+    public void Create_WhenCalledWithValidEntityObject_InsertsNewRecordIntoDatabase()
+    {
+        Mock<ILogger<AppRepository>> mockLogger = new Mock<ILogger<AppRepository>>();
+        var dbOptions = DatabaseCreator.CreateTestDatabase();
+        using (var testDbContext = new TestDatabaseContext(dbOptions))
+        {
+            AppRepository repo = new AppRepository(testDbContext, mockLogger.Object);
+            
+            //Logic pending
+
+
+            //Assert.True(employees.Count == 5);
+        }
+    }
+}
---
+++ Ps.CleanWebApi/Ps.CleanWebApi.Infrastructure.UnitTests/Update/Update_Tests.cs
@@ -0,0 +1,20 @@
+﻿namespace Ps.CleanWebApi.Infrastructure.UnitTests.Update;
+
+public class Update_Tests
+{
+    [Fact]
+    public void Update_WhenCalled_UpdatesTheRecordInTheDatabase()
+    {
+        Mock<ILogger<AppRepository>> mockLogger = new Mock<ILogger<AppRepository>>();
+        var dbOptions = DatabaseCreator.CreateTestDatabase();
+        using (var testDbContext = new TestDatabaseContext(dbOptions))
+        {
+            AppRepository repo = new AppRepository(testDbContext, mockLogger.Object);
+
+            //Logic pending
+
+
+            //Assert.True(employees.Count == 5);
+        }
+    }
+}
